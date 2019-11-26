d78978d40970f1fbe7b7d820533b9747c029f6e9
mike <mike.schoenbeck@gmail.com>
2019-11-26 18:34:18 -0500

    completed lab

--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/Pound.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/Pound.java
@@ -1,6 +1,14 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class Pound implements ConvertableCurrency {
+    @Override
+    public Double convert(CurrencyType currencyType) {
+        return currencyType.getRate() / CurrencyType.POUND.getRate();
+    }
+    public CurrencyType get(){
+        return CurrencyType.POUND;
+    }
 }
--- src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/currencies/USDollar.java
@@ -1,6 +1,14 @@
 package io.zipcoder.currencyconverterapplication.currencies;
 
 import io.zipcoder.currencyconverterapplication.ConvertableCurrency;
+import io.zipcoder.currencyconverterapplication.CurrencyType;
 
 public class USDollar implements ConvertableCurrency {
+    @Override
+    public Double convert(CurrencyType currencyType) {
+        return currencyType.getRate() / CurrencyType.US_DOLLAR.getRate();
+    }
+    public CurrencyType get(){
+        return CurrencyType.US_DOLLAR;
+    }
 }
